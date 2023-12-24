4690b39aa6b83899699ff878196cdc1fb55f4be1
Coddo621 <lovro.kacic1@gmail.com>
2023-12-24 13:14:10 +0100

    Backup before switch to QT

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Music-Player
-Python based music player
+Python based music player made using pygame for sound control and ctk(customtkinter) for UI
 
 ## Requirements
 To install all the required libraries run ``pip3 install -r requirements.txt``.
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from customtkinter import filedialog as fd
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("theme.json")
 
+ispaused = False
+
 class musicplayer(ctk.CTk):
     def __init__(self, root):
         self.root = root
@@ -14,8 +16,9 @@ class musicplayer(ctk.CTk):
         pygame.init()
         pygame.mixer.init()
         self.song = ctk.StringVar()
+        self.song.set("None")
         self.homedir = os.environ['HOME']
-        self.musicdir = self.homedir+"/Music"
+        self.musicdir = self.homedir+"/Music"  
         
         self.sidebar = ctk.CTkFrame(self.root, height = 800, width = 200)
         self.sidebar.place(x = 0, y = 0)
@@ -30,23 +33,50 @@ class musicplayer(ctk.CTk):
         self.openfile.place( x = 25, y = 25)
 
         def pauseunpause():
-            if pygame.mixer.get_busy():
-                pygame.mier.music.pause()
-            elif not pygame.mixer.music.get_busy():
+            global ispaused
+            if ispaused == False:
+                pygame.mixer.music.pause()
+                ispaused = True
+            elif ispaused == True:
                 pygame.mixer.music.unpause()
+                ispaused = False
 
         self.pausebutton = ctk.CTkButton(self.sidebar, text = "Pause", command = pauseunpause, font = ctk.CTkFont(size = 25))
         self.pausebutton.place(x = 25, y = 75)
 
-        self.songtrack = ctk.CTkLabel(self.root, textvariable = self.song, font = ctk.CTkFont(size = 50))
+        def stopsong():
+            pygame.mixer.music.stop
+            self.song = ""
+
+        self.stopbutton = ctk.CTkButton(self.sidebar, text = "Stop", command =  stopsong,font = ctk.CTkFont(size = 25))
+        self.stopbutton.place(x = 25, y = 125) 
+
+        self.songtrack = ctk.CTkLabel(self.root, textvariable = self.song, font = ctk.CTkFont(size = 50), text_color = "#D1D466")
         self.songtrack.place(x = 225, y = 25)
         
-        self.playlistframe = ctk.CTkFrame(self.root, width = 1000, height = 700)
+        self.playlistframe = ctk.CTkFrame(self.root, width = 1000, height = 575)
         self.playlistframe.place(x = 200, y = 100)
         self.playlistscroll = ctk.CTkScrollbar(self.root)
-        self.playlist = CTkListbox(self.playlistframe, width = 1000, height = 700, fg_color = "#DC3511", font = ("lexend", 20), border_width = 0)
+        self.playlist = CTkListbox(self.playlistframe, width = 1000, height = 575, fg_color = "#900C3F", font = ("lexend", 20), text_color = "#D1D466", border_width = 0)
         self.playlist.place(x = 0, y = 0)
-        
+
+        volumeget = ctk.StringVar
+        def changevolume(volume):
+            if(self.song == "None"):
+                print("No song is playing")
+            else:
+                pygame.mixer.music.set_volume(volume)
+
+                percent = volume*100
+                volumeget.set(value = volume)
+
+        self.volumeslider = ctk.CTkSlider(self.root, width = 1000, height = 25, command = changevolume)
+        self.volumeslider.set(1.0)
+        self.volumeslider.place(x = 200, y = 650)
+
+        self.volumelabel = ctk.CTkLabel(self.root, width = 50, height = 25, textvariable = volumeget)
+        self.volumelabel.place(x = 200, y = 725)
+
         os.chdir(self.musicdir)
         self.songlist = os.listdir()
         for song in self.songlist:
