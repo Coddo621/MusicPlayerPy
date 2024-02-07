336504496f2c679318fd47591301dcdcc92d88cb
Coddo621 <lovro.kacic1@gmail.com>
2024-02-07 21:45:17 +0100

    Fix pauseunpase and stop song error

--- main.py
+++ main.py
@@ -2,33 +2,40 @@ import tkinter
 import customtkinter as ctk
 import pygame
 import os
-import spotipy
+import threading
 from CTkListbox import *
 from customtkinter import filedialog as fd
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("theme.json")
-
-ispaused = False
+IsPaused = False
 
 class musicplayer(ctk.CTk):
     def __init__(self, root):
+        
+        #Initial root Tk setup
         self.root = root
         self.root.title("MuseX")
         self.root.geometry(f"{1200}x{800}")
         self.root.resizable(False, False)
+        #Redo icon implementation
+        #self.root.iconbitmap("icon.ico")
+        #print(os.path.abspath("icon.ico"))
+
+        #Initial Pygame Setup
         pygame.init()
         pygame.mixer.init()
+
+        #Control vars
         self.song = ctk.StringVar()
-        self.song.set("None")
-        self.homedir = os.environ['HOME']
-        self.musicdir = self.homedir+"/Music"  
+        #self.song.set("None")
+        self.PlaybackPosition = 0 
         
+        #Sidebar init
         self.sidebar = ctk.CTkFrame(self.root, height = 800, width = 200)
         self.sidebar.place(x = 0, y = 0)
-
         def getsong():
-            selectedsong = fd.askopenfilename(title = "Open file", initialdir = self.musicdir, filetypes = (("mp3 Files", "*.mp3"), ("aac Files", "*.acc"), ("FLAC Files", "*.flac")))
+            selectedsong = fd.askopenfilename(title = "Open file", initialdir = os.getcwd(), filetypes = (("mp3 Files", "*.mp3"), ("wav Files", "*.wav"), ("ogg Files", "*.ogg")))
             self.song.set(selectedsong)
             pygame.mixer.music.load(selectedsong)
             pygame.mixer.music.play()
@@ -37,23 +44,28 @@ class musicplayer(ctk.CTk):
         self.openfile.place( x = 25, y = 25)
 
         def pauseunpause():
-            global ispaused
-            if ispaused == False:
+            global IsPaused
+            if not IsPaused:
                 pygame.mixer.music.pause()
-                ispaused = True
-            elif ispaused == True:
+                self.PlaybackPosition = pygame.mixer.music.get_pos() / 1000
+                IsPaused = True
+            else:
                 pygame.mixer.music.unpause()
-                ispaused = False
+                pygame.mixer.music.set_pos(self.PlaybackPosition)
+                IsPaused = False
 
         self.pausebutton = ctk.CTkButton(self.sidebar, text = "Pause", command = pauseunpause, font = ctk.CTkFont(size = 25))
         self.pausebutton.place(x = 25, y = 75)
 
-        def stopsong():
-            pygame.mixer.music.stop
-            self.song = ""
+        #Stop Button
+        def StopFunc():
+            def stopsong():
+                pygame.mixer.music.stop()
+                self.song.set("")
 
-        self.stopbutton = ctk.CTkButton(self.sidebar, text = "Stop", command =  stopsong,font = ctk.CTkFont(size = 25))
-        self.stopbutton.place(x = 25, y = 125) 
+            self.stopbutton = ctk.CTkButton(self.sidebar, text = "Stop", command =  stopsong,font = ctk.CTkFont(size = 25))
+            self.stopbutton.place(x = 25, y = 125) 
+        StopFunc()
 
         self.songtrack = ctk.CTkLabel(self.root, textvariable = self.song, font = ctk.CTkFont(size = 50), text_color = "#D1D466")
         self.songtrack.place(x = 225, y = 25)
@@ -72,7 +84,7 @@ class musicplayer(ctk.CTk):
                 pygame.mixer.music.set_volume(volume)
 
                 percent = volume*100
-                volumeget.set(value = volume)
+                volumeget.set(value = percent)
 
         self.volumeslider = ctk.CTkSlider(self.root, width = 1000, height = 25, command = changevolume)
         self.volumeslider.set(1.0)
@@ -81,11 +93,17 @@ class musicplayer(ctk.CTk):
         self.volumelabel = ctk.CTkLabel(self.root, width = 50, height = 25, textvariable = volumeget)
         self.volumelabel.place(x = 200, y = 725)
 
-        os.chdir(self.musicdir)
-        self.songlist = os.listdir()
-        for song in self.songlist:
-            self.playlist.insert(ctk.END, song)
+        def ListSongs():
+            os.chdir(os.getcwd())
+            new_songlist = [song for song in os.listdir() if os.path.splitext(song)[1] in [".mp3", ".wav", ".ogg"]]
+            self.playlist.delete(0, ctk.END)
+            for song in new_songlist:
+                self.playlist.insert(ctk.END, song)
+            threading.Timer(5, ListSongs).start()
 
+        ListSongs()
+
+        self.playlist.get()
         def playsong(event):
             selectedsong = self.playlist.get(self.playlist.curselection())
             self.song.set(selectedsong)
@@ -96,5 +114,5 @@ class musicplayer(ctk.CTk):
 
 if __name__ == "__main__":
     root = ctk.CTk()
-    musicplayer(root)
+    app = musicplayer(root)
     root.mainloop()
